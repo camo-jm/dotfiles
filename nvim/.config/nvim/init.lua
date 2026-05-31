8c21001cf5327b9d67a80e9ad56e8b50a6a9f142
camo-jm <joaquinsmateos2@gmail.com>
2026-05-31 01:50:45 -0300

    added mason to nvim plugins

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,22 +1,20 @@
-vim.cmd('set tgc cul nowrap nu sb scs spr sta vb et list cc=80 ts=4 sw=4 so=10 siso=10 path+=**')
+vim.cmd('set tgc cul nowrap nu sb scs spr sta vb et list cc=80 ts=4 sw=4 so=10 siso=10 path+=** icm=split')
 vim.cmd('filetype plugin indent on')
-vim.g.doge_enable_mappings = 0
-vim.g.doge_doc_standard_python = 'google'
+vim.diagnostic.config({ virtual_text = true, signs = true, severity_sort = true })
 
 vim.pack.add({
 	'https://github.com/catppuccin/nvim',
 	'https://github.com/nvim-mini/mini.nvim',
 	'https://github.com/neovim/nvim-lspconfig',
-	'https://github.com/kkoomen/vim-doge', --TODO: doge#install() automation, or saying fuck it and changing the plugin
+    'https://github.com/mason-org/mason.nvim',
+    --'https://github.com/phrmendes/todotxt.nvim', --TODO: fix this or find an alternative
+    'https://github.com/kkoomen/vim-doge', --TODO: doge#install() automation, or saying fuck it and changing the plugin
 })
 
-vim.diagnostic.config({ virtual_text = true, signs = true, severity_sort = true })
+MINIS = {'files', 'move', 'pairs', 'surround', 'icons', 'statusline', 'tabline', 'bracketed', 'git', 'diff'}
+ENSURE_LSP = {'zls', 'lua-language-server', 'clangd', 'pyright'}
+ENABLE_LSP = {'zls', 'lua_ls', 'clangd', 'pyright'}
 
--- mini.nvim config
-Minis = {'files', 'move', 'pairs', 'surround', 'icons', 'statusline', 'tabline', 'bracketed', 'git', 'diff'}
-for _, m in ipairs(Minis) do
-	require('mini.' .. m).setup()
-end
 
 -- commands
 vim.cmd('colorscheme catppuccin')
@@ -24,32 +22,33 @@ vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader>f', ':lua MiniFiles.open()<CR>')
 vim.keymap.set('n', '<leader>t', ':80vsplit | te<CR>')
 vim.keymap.set('n', '<leader>b', ':lua require("mini.git").show_at_cursor()<CR>')
-vim.keymap.set("n", "<leader>c", "<Plug>(doge-generate)")
+vim.keymap.set('n', '<leader>c', '<Plug>(doge-generate)')
+vim.g.doge_enable_mappings = 0
+vim.g.doge_doc_standard_python = 'google'
 
--- LSP config: lua
-vim.lsp.config("lua_ls", {
-	filetypes = { "lua" },
+
+-- plugins config
+for _, m in ipairs(MINIS) do
+	require('mini.' .. m).setup()
+end
+
+require('mason').setup({ ensure_installed = ENSURE_LSP, })
+for _, server in pairs(ENABLE_LSP) do vim.lsp.enable(server) end
+
+
+-- LSP configs
+vim.lsp.config('lua_ls', {
+	filetypes = { 'lua' },
 	settings = {
 		telemetry = {enable = false},
 		Lua = {
-			diagnostics = {globals = { "vim" },},
-			workspace = { library = { "${3rd}/love2d/library"
-},},},},})
+			diagnostics = {globals = { 'vim' },},
+			workspace = { library = { '${3rd}/love2d/library'},},
+},},})
 
--- LSP config: typst
-vim.lsp.config("tinymist", {
-	cmd = { "tinymist" },
-	filetypes = { "typst" },
+vim.lsp.config('tinymist', {
+	cmd = { 'tinymist' },
+	filetypes = { 'typst' },
 	settings = {
-		formatterMode = "typstyle",
+		formatterMode = 'typstyle',
 },})
-
--- LSP loading
-for k, v in pairs({
-	pyright = { "python" },
-	lua_ls = { "lua" },
-	clangd = { "c", "cpp" },
-}) do
-	vim.lsp.config(k, { filetypes = v })
-	vim.lsp.enable(k)
-end
--- ytfzf/.config/ytfzf/conf.sh
+++ ytfzf/.config/ytfzf/conf.sh
@@ -1,5 +1,5 @@
-video_player=       "mpv"
-show_thumbnails=    1
-is_sort=            1           # show multiple videos
-search_source=      "youtube"
-video_pref=         "best"
+video_player="mpv"
+show_thumbnails=1
+is_sort=1 # show multiple videos
+search_source="youtube"
+video_pref="best"
